6f0a22a60191a7221b8a504c0c9581f94043c6f0
norel <norel78@gmail.com>
2025-06-03 12:38:42 +0200

    chore(data): ajout muséum national en lieu

--- data/lieux.sql
+++ data/lieux.sql
@@ -6,6 +6,10 @@ CREATE TABLE IF NOT EXISTS lieux (
     geom GEOMETRY(Point, 4326)
 );
 
+
+TRUNCATE TABLE lieux;
+
 INSERT INTO lieux (nom, geom) VALUES
 ('Tour Eiffel', ST_SetSRID(ST_MakePoint(2.2945, 48.8584), 4326)),
-('Louvre', ST_SetSRID(ST_MakePoint(2.3364, 48.8606), 4326));
+('Louvre', ST_SetSRID(ST_MakePoint(2.3364, 48.8606), 4326)),
+('Muséum national d’Histoire naturelle', ST_SetSRID(ST_MakePoint(2.3560490942326218, 48.84185618613116), 4326));
